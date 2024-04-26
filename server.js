eeb38751acd86059656dcc9d48b3b4d63f5b1eb8
AmineNaboulsi <naboulsiiamine@gmail.com>
2024-04-26 11:42:10 +0100

    Upload project to git 3

--- server.js
+++ server.js
@@ -13,18 +13,32 @@ global.con = mysql.createConnection({
   database: process.env._DB
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected to the database!");
+con.connect(function (err) {
+  if (err) {
+    console.log('error when connecting to db:', err);
+    setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+  } else {
+    console.log("Connected to the database!");
+  }
+
+});
+
+con.on('error', function(err) {
+  console.log('db error', err);
+  if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+    handleDisconnect();
+  } else {
+    throw err;
+  }
 });
 
 app.use("/users", userRouter);
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-    console.log("Environment Variables:");
+  console.log("Server is running on port 3000");
+  console.log("Environment Variables:");
   console.log("HOST:", process.env._HOST);
-console.log("USER:", process.env._USER);
-console.log("PASSWORD:", '[HIDDEN]'); // Do not print passwords
-console.log("DB:", process.env._DB);
+  console.log("USER:", process.env._USER);
+  console.log("PASSWORD:", '[HIDDEN]'); // Do not print passwords
+  console.log("DB:", process.env._DB);
 });
